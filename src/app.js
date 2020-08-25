7ecb848fe17a596261ca4264f6c68f5bb156c957
Daniel B <danielbuompane@gmail.com>
2020-08-24 23:09:07 -0400

    remove API Key

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const request = require("postman-request");
-const PHISHIN_API_KEY = `4ebb30d807dfd43cfb4b3941ce4b22323accae11d0f256cc857f46f3f128d7b8f423c96ccab292bbe6715501dcd6aac6`;
+
 const jamchartFilter = require("../utils/jamchartFilter");
 const url = ` http://phish.in/api/v1/years/1997.json`;
 const options = {
