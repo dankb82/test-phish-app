f7de85d9547d1eb961dce6e182557e9268dbac86
Daniel B <danielbuompane@gmail.com>
2020-08-26 18:09:49 -0400

    Stand up express server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const request = require("postman-request");
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 require("dotenv").config();
 const jamchartFilter = require("../utils/jamchartFilter");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Phish app!");
+});
+
 const url = ` http://phish.in/api/v1/years/1997.json`;
 const options = {
   url,
@@ -21,4 +31,8 @@ request(options, (error, { body }) => {
   });
 
   const jamchartVersions = jamchartFilter(allTracks);
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
 });
