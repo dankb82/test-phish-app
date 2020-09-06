7caa7ed4c6241610af7a12fcf8a07c6e3389c3d6
Daniel B <danielbuompane@gmail.com>
2020-09-06 18:25:45 -0400

    Switch to Axios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const request = require("postman-request");
+const axios = require("axios");
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
@@ -20,26 +20,39 @@ app.use(express.static(PUBLIC_DIRECTORY_PATH));
 
 const url = `http://phish.in/api/v1/random-show`;
 
-const options = {
-  url,
-  json: true,
-  headers: {
-    Accept: "application/json",
-    Authorization: `Bearer ${process.env.PHISHIN_API_KEY}`,
-  },
+const randomShowRequest = async () => {
+  return axios.get(url, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${process.env.PHISHIN_API_KEY}`,
+    },
+  });
 };
 
-app.get("", (req, res) => {
-  request(options, (error, { body }) => {
-    const date = body.data.date;
-    const venue = body.data.venue.name;
-    const location = body.data.venue.location;
-    res.render("index", {
+app.get("", async (req, res) => {
+  try {
+    /*
+    Destructuring API response several levels deep for now to pull out the nested values. 
+    This is initially to avoid having to dig too deeply into a heavily nested object more than once, 
+    but it may prove to be an unnecessary readability tradeoff. 
+    */
+    const {
+      data: {
+        data: { date },
+      },
+      data: {
+        data: { venue },
+      },
+    } = await randomShowRequest();
+    const responseBody = {
       date,
-      venue,
-      location,
-    });
-  });
+      venue: venue.name,
+      location: venue.location,
+    };
+    res.render("index", responseBody);
+  } catch (e) {
+    console.log(e.stack);
+  }
 });
 
 app.listen(3000, () => {
